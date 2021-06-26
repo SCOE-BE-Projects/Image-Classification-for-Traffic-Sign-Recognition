017866af181a75b0a1ab726ec0305c2490aba961
vedantMahangade <vedantmahangade@outlook.com>
2021-06-26 19:30:10 +0530

    Commented Tensorflow logs

--- classify.py
+++ classify.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import time
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 import tensorflow as tf
 from keras.models import load_model
 
